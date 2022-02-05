bae1f2b47cafed0987d78d494154aedf2862607f
Tao-X02 <68438230+Tao-X02@users.noreply.github.com>
2022-02-05 18:19:51 -0500

    Python functions to label image with gene name

---
+++ label_image.py
@@ -0,0 +1,35 @@
+import re
+import cv2
+import pytesseract
+from pytesseract import Output
+import os
+
+def check_similarity(a, b):
+    chars_to_remove = "1ltLT"
+    for char in chars_to_remove:
+        a = a.replace(char, "")
+        b = b.replace(char, "")
+    if (a in b):
+        return True
+    return False
+
+def label_image(file_name):
+    gene_name = os.path.basename(file_name).split(".")[0].split("_")[0]
+    
+    # Check if found
+    found = False
+    
+    img = cv2.imread(file_name)
+    d = pytesseract.image_to_data(img, output_type=Output.DICT)
+    keys = list(d.keys())
+    
+    # Iterate
+    n_boxes = len(d['text'])
+    for i in range(n_boxes):
+        if check_similarity(gene_name, d['text'][i]):
+            found = True;
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            img = cv2.rectangle(img, (x - 6, y - 6), (x + w + 9, y + h + 9), (0, 255, 0), 2)
+        
+    cv2.imwrite('output.png', img) # Modify this
+    return found
