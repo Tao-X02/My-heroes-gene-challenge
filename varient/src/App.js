b51772cdd519fa18537f8ccdc101e4272ff7d56a
Taoguang Xing <taoguangxing@gmail.com>
2022-02-04 22:58:12 -0500

    initial routing

--- varient/src/App.js
+++ varient/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+import Signup from './Pages/Signup';
+import Summary from './Pages/Summary';
+import Setting from './Pages/Setting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login/>}/>
+        <Route exact path="/Dashboard" element={<Dashboard/>}/>
+        <Route exact path="/Signup" element={<Signup/>}/>
+        <Route exact path="/Summary" element={<Summary/>}/>
+        <Route exact path="/Setting" element={<Setting/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
